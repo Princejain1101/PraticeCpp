cf689171f396b00b56a4a776e2879296fb070034
princejain1101 <prince.jain@intel.com>
2019-06-02 22:32:44 -0700

    Initial Commit

---
+++ leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by Prince Jain on 6/2/19.
+//  Copyright © 2019 Prince Jain. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
